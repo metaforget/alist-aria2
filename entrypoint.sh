6470ee6173938b1e4206d67f26899e0b1680e0a8
metaforget <1@gmail.com>
2024-06-22 23:05:11 +0800

    修改aria2

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,7 @@ chown -R ${PUID}:${PGID} /opt/alist/
 umask ${UMASK}
 
 exec su-exec ${PUID}:${PGID} nohup aria2c \
-  --enable-rpc \
-  --rpc-allow-origin-all \
   --conf-path=/opt/alist/data/aria2/aria2.conf \
-  >/dev/null 2>&1 &
+  >/opt/alist/data/log/aria2.log 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
